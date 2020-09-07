f65d3f396c1df293f26ff99ee0dcb6c5c2632713
Julius <Julius>
2020-09-07 13:13:48 +0300

    add views

--- main.js
+++ main.js
@@ -253,8 +253,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\wordpress\wp-content\themes\front\src\app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\wordpress\wp-content\themes\front\src\app.scss */"./src/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/wordpress/wp-content/themes/BIT-first-theme/src/app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/wordpress/wp-content/themes/BIT-first-theme/src/app.scss */"./src/app.scss");
 
 
 /***/ })
